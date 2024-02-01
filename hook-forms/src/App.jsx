522604cf4cd5ea0f6c22dc0376bd6aca64a909a9
Anavi-K <anavikothari@outlook.com>
2024-02-01 09:20:29 +0530

    updated

--- hook-forms/src/App.jsx
+++ hook-forms/src/App.jsx
@@ -1,113 +1,69 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 export default function App() {
-  const [field, setField] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: "",
-  });
-
-  const [submitted, setSubmit] = useState(false);
-  const [validate, setValidation] = useState(false);
-
-  const validateEmail = (email) => {
-    return email.includes("@");
-  };
-
-  const validateName = (name) => {
-    const nameRegex = /^[a-zA-Z.]{2,}$/;
-    return nameRegex.test(name);
-  };
-
-  const validatePassword = (password) => {
-    return password.length >= 4 && password.length <= 20;
+  const { register, handleSubmit, setValue, formState: { errors, isValid } } = useForm();
+  const [isButtonClicked, setIsButtonClicked] = useState(false);
+  const onSubmit = (data) => {
+    console.log("Form data:", data);
+    if (isValid) {
+      setIsButtonClicked(true);
+    } else {
+      setIsButtonClicked(false);
+    }
   };
 
   return (
     <div className="form-container">
-      <form
-        className="register-form"
-        onSubmit={(e) => {
-          e.preventDefault();
-          if (
-            validateName(field.firstName) &&
-            validateName(field.lastName) &&
-            field.email &&
-            validateEmail(field.email) &&
-            field.password && 
-            validatePassword(field.password)
-          ) {
-            setValidation(true);
-          }
-          setSubmit(true);
-        }}
-      >
-        {submitted && validate ? (
-          <div className="success-message">Registration successful!</div>
-        ) : null}
+      {isButtonClicked && isValid && (
+        <div className="success-message">Registration successful!</div>
+      )}
 
+      <form className="register-form" onSubmit={handleSubmit(onSubmit)}>
         <input
           id="first-name"
           className="form-field"
           type="text"
           placeholder="First Name"
-          name="firstName"
-          value={field.firstName}
-          onChange={(e) => {
-            setField({ ...field, firstName: e.target.value });
-          }}
+          {...register("firstName", { required: true, pattern: /^[a-zA-Z.]{2,}$/ })}
         />
-        {submitted && (!field.firstName || !validateName(field.firstName)) ? (
+        {errors.firstName && (
           <span>Please enter a valid first name</span>
-        ) : null}
+        )}
 
         <input
           id="last-name"
           className="form-field"
           type="text"
           placeholder="Last Name"
-          name="lastName"
-          value={field.lastName}
-          onChange={(e) => {
-            setField({ ...field, lastName: e.target.value });
-          }}
+          {...register("lastName", { required: true, pattern: /^[a-zA-Z.]{2,}$/ })}
         />
-        {submitted && (!field.lastName || !validateName(field.lastName)) ? (
+        {errors.lastName && (
           <span>Please enter a valid last name</span>
-        ) : null}
+        )}
 
         <input
           id="email"
           className="form-field"
           type="text"
           placeholder="Email"
-          name="email"
-          value={field.email}
-          onChange={(e) => {
-            setField({ ...field, email: e.target.value });
-          }}
+          {...register("email", { required: true, pattern: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/ })}
         />
-        {submitted && !field.email ? <span>Please enter your email</span> : null}
-        {submitted && field.email && !validateEmail(field.email) ? (
+        {errors.email && (
           <span>Please enter a valid email address</span>
-        ) : null}
+        )}
 
         <input
           id="password"
           className="form-field"
-          type="password" 
+          type="password"
           placeholder="Password"
-          name="password"
-          value={field.password}
-          onChange={(e) => {
-            setField({ ...field, password: e.target.value });
-          }}
+          {...register("password", { required: true, minLength: 4, maxLength: 20 })}
         />
-        {submitted && (!field.password || !validatePassword(field.password)) ? (
+        {errors.password && (
           <span>Password must be between 4 and 20 characters</span>
-        ) : null}
+        )}
 
         <button className="form-field" type="submit">
           Register
